4e4d83f366b9b5ffeaf7011001f0856798eb3f34
Hunter Akeriano <gamerstaject@gmail.com>
2022-12-11 17:13:48 +0200

    add my works

--- js/script.js
+++ js/script.js
@@ -68,5 +68,63 @@ for (let elm of elements) {
   observer.observe(elm);
 };
 
+let swiper = new Swiper(".swiper1",{
+    // spaceBetween: 30,
+  
+    centerSlides: 'true',
+    fade: 'true',
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            centerSlides: 'true',
+            loop: true,
+        },
+        950: {
+            slidesPerView: 2,
+            loop: true,
+        },
+        1095: {
+            slidesPerView: 4,
+            loop: true,
+        },
+        1290:{
+          slidesPerView: 5,
+        }
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+     
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+
+  });
 
 
+// tabs
+
+const tabsBtn = document.querySelectorAll('.swiper-btn');
+const tabsItems = document.querySelectorAll('.works__content')
+
+tabsBtn.forEach(function(item){
+  item.addEventListener('click', function(){
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+   
+
+    tabsBtn.forEach(function(item){
+      item.classList.remove('active')
+    });
+
+    tabsItems.forEach(function(item){
+      item.classList.remove('active')
+    });
+
+    currentBtn.classList.add('active');
+    currentTab.classList.add('active');
+  })
+})
